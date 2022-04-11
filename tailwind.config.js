889b2f720a97da929b714bc0f9ef0f316bb44113
carolchyang <chingheui.yang@gmail.com>
2022-04-11 19:21:31 +0800

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{html,js}"
+  ],
+  theme: {
+    fontFamily: {
+      'sans': ['Noto Sans TC', 'Roboto', 'Arial', 'sans-serif'],
+    },
+    extend: {
+      colors: {
+        'muted': '#CED4DA',
+        'dark': '#212529',
+      },
+    },
+  },
+  plugins: [],
+}
